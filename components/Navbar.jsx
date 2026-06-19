a6a0eb19d96a9e6a97c5cc01c575632954e02bf0
Speed <speedmw14@gmail.com>
2026-06-19 13:43:46 -0600

    Agregar logo en navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,17 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <Link href="/" className="logo">
-        Habitta
+      <Link href="/" className="navbar-logo">
+        <Image
+          src="/logo.png"
+          alt="Habitta"
+          width={140}
+          height={60}
+          priority
+        />
       </Link>
 
       <div>
